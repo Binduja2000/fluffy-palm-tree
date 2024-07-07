20b3cd847d0ee3ba39498553530cb2082a1e43ad
Binduja <100854763+Binduja2000@users.noreply.github.com>
2024-07-07 18:56:16 +0530

    Add files via upload

---
+++ ASP.NET Project/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using sample_web_application.Models;
+
+namespace sample_web_application.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<sample_web_application.Models.Movie> Movie { get; set; }            }
+}
